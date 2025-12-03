9821129f823cb3c877bf46b409714baba7fe6cee
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-03 21:00:21 +0300

    Добавил определение метода check для UniquenessRule

--- validator/rules.js
+++ validator/rules.js
@@ -5,7 +5,19 @@ class Rule {
 
 
 class UniquenessRule extends Rule {
-    check(componentManager) {
-        return false;
+    check(componentManager, componentClass, newComponentName="") {
+        let repository = componentManager.copyRepository(componentClass);
+        
+        for (let component of repository.values()) {
+            
+            if (component.name === newComponentName && component.name !== '') {
+                if (componentClass === Process)
+                    return component.iteration !== 0;
+                return false;
+            }
+            
+        }
+
+        return true;
     }
 }
