0fe167fa8b2a3b3c28d4494af9ba34035a7e0922
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-04 14:35:04 +0300

    Реализовал метод explainError

--- validator/rules.js
+++ validator/rules.js
@@ -4,6 +4,9 @@ class Rule {
 }
 
 class UniquenessRule extends Rule {
+    #sameIterationError = false;
+    #sameTypeError = false;
+    
     check(componentManager, componentClass, componentName='') {
         if (componentName === '') return true;
         
@@ -12,9 +15,32 @@ class UniquenessRule extends Rule {
         if (duplicate === undefined)
             return true;
         else {
-            if (componentClass === Process)
-                return !(duplicate.iteration === 0);
-            return !(duplicate instanceof componentClass);
+            if (componentClass === Process) {
+                if (duplicate.iteration === 0) {
+                    this.#sameIterationError = true;
+                    return false;
+                }
+                else
+                    return true;
+            }
+
+            if (duplicate instanceof componentClass) {
+                this.#sameTypeError = true;
+                return false;
+            }
+            else
+                return true;
         }
+    }
+
+    explainError() {
+        let explanation = 'Ошибка правила уникальности:';
+
+        if (this.#sameIterationError)
+            explanation += '\n- Процессы с одинаковыми названиями могут существовать только в разных итерациях';
+        if (this.#sameTypeError)
+            explanation += '\n- Компоненты с одинаковыми названиями могут существовать, только если они разных типов';
+
+        return explanation;
     }
 }
