e966231c51f2c5385092e39465415162d16f9e6d
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-16 14:43:19 +0300

    Добавил тесты для RuleSet

---
+++ validator/test.ruleset.js
@@ -0,0 +1,36 @@
+describe('RuleSet', function(){
+
+    it('returns undefined when no rules were performed', function(){
+        const set = new RuleSet();
+
+        assert.isUndefined(set.check());
+    });
+    
+    it('takes set definition and runs it', function(){
+        const manager = new ComponentManager();
+        
+        const set = new RuleSet([
+            [UniquenessRule, (r) => r.check(manager, Process, 'p1')],
+            [NestingRule, (r) => r.check(Process, Element)]
+        ]);
+
+        const checkResult = set.check();
+        assert.isTrue(checkResult);
+    });
+
+    it('returns last failed rule', function(){
+        const manager = new ComponentManager();
+
+        manager.createComponent(Process, '', 'p1');
+
+        const set = new RuleSet([
+            [UniquenessRule, (r) => r.check(manager, Process, 'p1')],
+            [NestingRule, (r) => r.check(Process, Element)]
+        ]);
+
+        const checkResult = set.check();
+//        assert.isTrue(checkResult.isFailed());
+        assert.instanceOf(checkResult, UniquenessRule);
+    });
+    
+});
