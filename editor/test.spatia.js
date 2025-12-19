ba585db72771ee7bcb2d6f1060974738d2395602
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-19 14:02:37 +0300

    Добавил тесты для Spatia

---
+++ editor/test.spatia.js
@@ -0,0 +1,49 @@
+describe('Spatia', function(){
+
+    it('returns `true` if target reachable for cursor', function(){
+        const spatia = new Spatia(1, 0.3);
+        const target = new Point(1, 1);
+        const cursor = new Point(1.2, 0.8);
+
+        assert.isTrue(spatia.isReachable(target, cursor));
+    });
+
+    it('returns `false` if target not reachable for cursor', function(){
+        const spatia = new Spatia(1, 0.3);
+        const target = new Point(1, 1);
+        const cursor = new Point(1.4, 0.6);
+
+        assert.isFalse(spatia.isReachable(target, cursor));
+    });
+    
+    it('snaps reachable point', function(){
+        const spatia = new Spatia(1, 0.2);
+        const cursor = new Point(0.9, 1.1);
+
+        const snappedCusor = spatia.snapPoint(cursor);
+
+        assert.equal(snappedCusor.X, 1);
+        assert.equal(snappedCusor.Y, 1);
+    });
+
+    it('does not snap unreachable point', function(){
+        const spatia = new Spatia(1, 0.2);
+        const cursor = new Point(0.7, 1.3);
+
+        const snappedCusor = spatia.snapPoint(cursor);
+
+        assert.equal(snappedCusor.X, 0.7);
+        assert.equal(snappedCusor.Y, 1.3);
+    });
+
+    it('does not snap middle point', function(){
+        const spatia = new Spatia(4, 2);
+        const cursor = new Point(2, 0);
+
+        const snappedCusor = spatia.snapPoint(cursor);
+
+        assert.equal(snappedCusor.X, 2);
+        assert.equal(snappedCusor.Y, 0);
+    });
+
+});
