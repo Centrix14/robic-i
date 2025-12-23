608a2293989d6e5069017793d0587c6b0213a9d4
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-23 13:07:33 +0300

    Добавил проверку системы стилей

--- editor/test.primitives.js
+++ editor/test.primitives.js
@@ -56,17 +56,75 @@ describe('Point', function() {
 
 describe('Figure', function() {
 
+    it('can use style on SVGElement', function(){
+        const style = new SkeletonStyle('test', new Stroke('blue', '1', '', 'miter'));
+        
+        const set = new StyleSet();
+        set.add(style);
+
+        const figure = new Figure('1', '');
+        figure.styleSet = set;
+
+        const elm = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        figure.useStyle('test', elm);
+
+        assert.equal(elm.getAttribute('stroke'), 'blue');
+        assert.equal(elm.getAttribute('stroke-width'), '1');
+        assert.equal(elm.getAttribute('stroke-dasharray'), '');
+        assert.equal(elm.getAttribute('stroke-linejoin'), 'miter');
+    });
+
+});
+
+describe('FigureManager', function(){
+
+    it('adds style sets', function(){
+        const set1 = {
+            name: 'set1'
+        }
+        const set2 = {
+            name: 'set2'
+        }
+        
+        const manager = new FigureManager();
+        manager.addStyleSet(set1);
+        manager.addStyleSet(set2);
+
+        assert.lengthOf(manager.styleSets, 2);
+    });
+
+    it('ejects style sets', function(){
+        const set1 = {
+            name: 'set1'
+        }
+        const set2 = {
+            name: 'set2'
+        }
+
+        const manager = new FigureManager();
+        manager.addStyleSet(set1);
+        manager.addStyleSet(set2);
+
+        assert.equal(manager.ejectStyleSet('set1').name, 'set1');
+        assert.equal(manager.ejectStyleSet('set2').name, 'set2');
+        assert.isEmpty(manager.styleSets);
+    });
+
 });
 
 describe('Rect', function() {
 
     describe('serialize', function() {
-        let rect1 = Rect.createByMeasures(2, '', new Point(1,1), 50, 50);
-        let rect2 = Rect.createByPoints(1, '', new Point(1,1), new Point(51,51));
 
         it('takes only SVGElement', function() {
             let svgElm = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
             let htmlElm = document.createElement('div');
+
+            const rect1 = Rect.createByMeasures(2, '', new Point(1,1), 50, 50);
+            rect1.styleSet = {
+                name: 'test',
+                useOn: () => {}
+            };
 
             assert.isTrue(rect1.serialize(svgElm).isSuccess() &&
                           rect1.serialize(htmlElm).hasErrors());
@@ -74,7 +132,15 @@ describe('Rect', function() {
 
         it('sets `x`, `y`, `width` and `height` attributes', function() {
             let elm = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+
+            const rect2 = Rect.createByPoints(1, '', new Point(1,1), new Point(51,51));
+            rect2.styleSet = {
+                name: 'test',
+                useOn: () => {}
+            };
+            
             rect2.serialize(elm);
+
             assert.isTrue(elm.getAttribute('x') === '1' &&
                           elm.getAttribute('y') === '1' &&
                           elm.getAttribute('width') === '50' &&
