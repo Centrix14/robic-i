b0adb258e97d5fa70778b682c5119936811b5522
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-24 11:15:16 +0300

    Начал писать менеджер событий

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ function canvasCoords(x, y) {
     return _point.matrixTransform(canvas.getScreenCTM().inverse());
 }
 
+function adhocMouseEvent(name, event) {
+    return {
+        name,
+        button: event.button,
+        x: event.x,
+        y: event.y
+    };
+}
+
+function adhocKeyboardEvent(name, event) {
+    return {
+        name,
+        key: event.key
+    };
+}
+
+function isEven(x) {
+    return !(x & 1);
+}
+
 class StatusBar {
     #element = undefined;
 
@@ -27,6 +47,8 @@ class Application {
 
     #canvas = undefined;
 
+    #eventBuffer = [];
+
     constructor() {
         this.#roles = new ElementRole();
         this.#roles.create('none');
@@ -45,15 +67,85 @@ class Application {
     }
 
     canvasSelect(event) {
-        const cursor = canvasCoords(event.clientX, event.clientY);
+        const cursor = canvasCoords(event.x, event.y);
         this.#editor.select(cursor.x, cursor.y);
+    }
+
+    canvasClick(event) {
+        this.canvasSelect(event);
+    }
+
+    eventDispatch() {
+        const events = this.#eventBuffer;
+
+        if (isEven(events.length)) {
+            const first = events.length - 2;
+            const second = events.length - 1;
+
+            if (events[first].name === 'mousedown' && events[second].name === 'mouseup') {
+                this.canvasClick(events[0]);
+                this.#eventBuffer = [];
+            }
+            if (events[first].name === 'keyDown' && events[second].name === 'keyUp') {
+                switch (events[0].key) {
+                case 'Escape':
+                    break;
+                }
+                
+                this.#eventBuffer = [];
+            }
+        }
+    }
+
+    mouseDown(event) {
+        this.#eventBuffer.push(adhocMouseEvent('mousedown', event));
+        this.eventDispatch();
+    }
+
+    mouseUp(event) {
+        this.#eventBuffer.push(adhocMouseEvent('mouseup', event));
+        this.eventDispatch();
+    }
+
+    keyDown(event) {
+        this.#eventBuffer.push(adhocKeyboardEvent('keyDown', event));
+        this.eventDispatch();
+    }
+
+    keyUp(event) {
+        this.#eventBuffer.push(adhocKeyboardEvent('keyUp', event));
+        this.eventDispatch();
     }
 }
 
 const app = new Application();
 const statusBar = new StatusBar(document.getElementById('status-text'));
 
-canvas.addEventListener('click', (event) => app.canvasSelect(event));
+//canvas.addEventListener('click', (event) => app.canvasSelect(event));
+//canvas.addEventListener('click', function(event){
+//    statusBar.print('Click!');
+//});
+
+canvas.addEventListener('pointerdown', function(event){
+    statusBar.print('Down');
+    app.mouseDown(event);
+});
+
+canvas.addEventListener('pointerup', function(event){
+    statusBar.print('Up');
+    app.mouseUp(event);
+});
+
 canvas.addEventListener('mousemove', function(event) {
-    statusBar.print(event.movementX + ', ' + event.movementY);
-})
+    //statusBar.print(event.movementX + ', ' + event.movementY);
+});
+
+const body = document.querySelector('body');
+
+body.addEventListener('keydown', function(event){
+    app.keyDown(event);
+});
+
+body.addEventListener('keyup', function(event){
+    app.keyUp(event);
+});
