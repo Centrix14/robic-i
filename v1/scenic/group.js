1fafdfd814c47aed24a3842389419228b4bb3b7c
Centrix14 <artiom.zvyagin@yandex.ru>
2026-03-14 16:50:55 +0300

    ~fn(init): рефакторинг init

--- v1/scenic/group.js
+++ v1/scenic/group.js
@@ -21,36 +21,27 @@ class Group {
 
 class ProcessGroup extends Group {
     init(id, operator) {
-        const shape = new Rect(new Point(10, 10), 100, 50);
-        this._store.set('shape', shape);
-
-        const shapeElm = operator.createRect();
-        operator.applyTo(shapeElm, {
-            id: 'shape',
-            ...shape
-        });
-
-        const name = new Text(new Point(55, 35), 'Процесс');
-        this._store.set('name', name);
-        const nameElm = operator.createText();
-        operator.applyTo(nameElm, {
-            id: 'name',
-            ...name
-        });
-
-        const designation = new Text(new Point(100, 50), `П ${id}`);
-        this._store.set('designation', designation);
-        const designationElm = operator.createText();
-        operator.applyTo(designationElm, {
-            id: 'designation',
-            ...designation
-        });
-
         const group = operator.createGroup();
         operator.applyTo(group, { id });
-        operator.appendChild(group, shapeElm);
-        operator.appendChild(group, nameElm);
-        operator.appendChild(group, designationElm);
+
+        const store = this._store;
+        store.set('shape', [
+            new Rect(new Point(10, 10), 100, 50),
+            operator.createRect()
+        ]);
+        store.set('name', [
+            new Text(new Point(55, 35), 'Процесс'),
+            operator.createText()
+        ]);
+        store.set('designation', [
+            new Text(new Point(100, 50), `П ${id}`),
+            operator.createText()
+        ]);
+
+        for (let [id, [figure, element]] of store) {
+            operator.applyTo(element, { id, ...figure });
+            operator.appendChild(group, element);
+        }
 
         return group;
     }
