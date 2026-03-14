edca873d71903a07cf972dba8f11d5e37e8d8ff5
Centrix14 <artiom.zvyagin@yandex.ru>
2026-03-14 15:58:06 +0300

    +test(init2) ~test(init1): должен применять к элементам свойства

--- v1/scenic/test.js
+++ v1/scenic/test.js
@@ -8,7 +8,8 @@ describe('ProcessGroup', function(){
                 texts: 0,
 
                 createRect: () => operator.rects++,
-                createText: () => operator.texts++
+                createText: () => operator.texts++,
+                applyTo: () => {}
             };
 
             const pg = new ProcessGroup();
@@ -16,6 +17,28 @@ describe('ProcessGroup', function(){
 
             assert.equal(operator.rects, 1, 'Rect creation failure');
             assert.equal(operator.texts, 2, 'Texts creation failure');
+        });
+
+        it('init2 - applying properties to SVG elements', function(){
+            const operator = {
+                rects: 0,
+                texts: 0,
+
+                isOk: false,
+
+                createRect: () => operator.rects++,
+                createText: () => operator.texts++,
+
+                applyTo: function(element, definition){
+                    if ((element === 1 || element === 2) && definition)
+                        operator.isOk = true;
+                }
+            };
+
+            const pg = new ProcessGroup();
+            pg.init(operator);
+
+            assert.isTrue(operator.isOk);
         });
 
     });
