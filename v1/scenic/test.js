11b97b53f5597f91806cf1f13dc7755ed9d15033
Centrix14 <artiom.zvyagin@yandex.ru>
2026-03-17 15:39:56 +0300

    ~test(NaiveStepLine3), +test(NaiveStepLine4): тест на инверсию

--- v1/scenic/test.js
+++ v1/scenic/test.js
@@ -114,8 +114,15 @@ describe('NaiveStepLine', function(){
     it('NaiveStepLine3 - makes step regular line', function(){
         const line = new NaiveStepLine(new Point(10, 10), new Point(50, 50));
         const obj = line.publish();
-        assert.equal('M 10 10 h 25 M 25 10 v 40 M 25 50 h 25', obj.d,
+        assert.equal('M 10 10 h 20 M 20 10 v 40 M 20 50 h 20', obj.d,
                      'Incorrect path');
-    })
+    });
+
+    it('NaiveStepLine4 - makes inversed step line', function(){
+        const line = new NaiveStepLine(new Point(50, 50), new Point(10, 10));
+        const obj = line.publish();
+        assert.equal('M 50 50 h -25 M 25 50 v -40 M 25 10 h -25', obj.d,
+                     'Incorrect path');
+    });
 
 });
