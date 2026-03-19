b10d4a8990e0b33f4786881b2771bb99c1867f33
Centrix14 <artiom.zvyagin@yandex.ru>
2026-03-19 17:04:44 +0300

    +test= isReachable(pp) 1-3

--- v1/scenic/test.js
+++ v1/scenic/test.js
@@ -276,6 +276,39 @@ describe('ProcessGroup', function(){
 
 describe('Spatia', function(){
 
+    describe('isReachable(target: point, cursor: Point)', function(){
+        const s = new Spatia(),
+              p = new Point(1,1);
+
+        it('isReachable(pp) 1 - detects direct hit', function(){
+            assert.isTrue(s.isReachable(p, new Point(1,1)),
+                         'Direct hit not detected');
+        });
+
+        it('isReachable(pp) 2 - detects hit in precision area', function(){
+            assert.isTrue(s.isReachable(p, new Point(2,3)),
+                          'Hit @ 2,3 not detected');
+            assert.isTrue(s.isReachable(p, new Point(-3,3)),
+                          'Hit @ -3,3 not detected');
+            assert.isTrue(s.isReachable(p, new Point(-2,-1)),
+                          'Hit @ -2,-1 not detected');
+            assert.isTrue(s.isReachable(p, new Point(1,-1)),
+                          'Hit @ 1,-1 not detected');
+        });
+
+        it('isReachable(pp) 3 - not detects hit in outside precision area',
+           function(){
+               assert.isFalse(s.isReachable(p, new Point(4,6)),
+                              'Hit @ 4,6 detected');
+               assert.isFalse(s.isReachable(p, new Point(-5,1)),
+                              'Hit @ -5,1 detected');
+               assert.isFalse(s.isReachable(p, new Point(-3,-3)),
+                              'Hit @ -3,-3 detected');
+               assert.isFalse(s.isReachable(p, new Point(7,-1)),
+                              'Hit @ 7,-1 not detected');
+           });
+    });
+
     describe('isReachable(target:Line, cursor:Point)', function(){
         const s = new Spatia(),
               l = new StraightLine(new Point(1,1), new Point(5,5));
