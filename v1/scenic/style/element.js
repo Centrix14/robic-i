e6ea37b91bf8c1631f4960f3d1de8b0fb436962d
Centrix14 <artiom.zvyagin@yandex.ru>
2026-03-23 16:54:24 +0300

    ~file ; разделил модуль элементов стиля и стиля

--- v1/scenic/style/element.js
+++ v1/scenic/style/element.js
@@ -1,68 +1,3 @@
-class Style {
-    constructor() {
-        this._index = 0;
-        this._store = new Map();
-    }
-
-    get(id) {
-        return this._store.get(id);
-    }
-
-    add(element, id=null) {
-        this._store.set(id ?? this._index, element);
-
-        if (id)
-            return id;
-        else
-            return this._index++;
-    }
-
-    drop(id) {
-        const element = this._store.delete(id);
-        if (element)
-            return new Success([['element', element]]);
-        else
-            return new Fail();
-    }
-
-    useOn(target) {
-        this._store.forEach((element) => element.useOn(target));
-    }
-
-    static build(stylesheet) {
-        const style = new Style();
-
-        for (let key in stylesheet) {
-            const definition = stylesheet[key];
-
-            let element;
-            switch (key) {
-            case 'stroke':
-                element = new Stroke(definition);
-                break;
-            case 'fill':
-                element = new Fill(definition);
-                break;
-            case 'font':
-                element = new Font(definition);
-                break;
-            case 'textAlign':
-                element = new TextAlign(definition);
-                break;
-            case 'marker':
-                element = new Marker(definition);
-                break;
-            default:
-                return new Fail();
-            }
-
-            style.add(element, key);
-        }
-
-        return style;
-    }
-}
-
 class StyleElement {
     useOn(target, options) {}
 }
@@ -188,4 +123,3 @@ class Marker extends StyleElement {
         target.setAttribute('marker-end', this._end);
     }
 }
-
