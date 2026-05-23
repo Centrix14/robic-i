48577309ff34d43634486ff5c121ecc8e17ba70a
Centrix14 <artiom.zvyagin@yandex.ru>
2026-05-23 09:46:53 +0300

    +fn= ElementExport.toCSV ; создаёт CSV-таблицу из массива

--- v1/export/ElementExport.js
+++ v1/export/ElementExport.js
@@ -1,4 +1,23 @@
 class ElementExport {
+    static toCSV(table) {
+        let csvTable = '';
+
+        for (let row of table) {
+            let csvRow = '';
+
+            for (let column of row) {
+                if (typeof column === 'string')
+                    csvRow += `\"${column}\",`;
+                else
+                    csvRow += `${column},`;
+            }
+
+            csvTable += csvRow.slice(0, csvRow.length-1) + '\n';
+        }
+
+        return csvTable;
+    }
+
     constructor(settings) {
         this._settings = settings;
         this.clear();
