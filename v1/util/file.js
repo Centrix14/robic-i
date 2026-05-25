f4bcfd1c34f1b7f118e407adb6f02b973a5dece6
Centrix14 <artiom.zvyagin@yandex.ru>
2026-05-25 11:31:57 +0300

    +file ; модуль утилит файлов

---
+++ v1/util/file.js
@@ -0,0 +1,29 @@
+function openIdf(callback) {
+    const input = document.createElement('input');
+
+    input.type = 'file';
+    input.multiple = false;
+
+    input.onchange = (event) => {
+        const file = event.target.files[0];
+
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            callback(event.target.result);
+        };
+        reader.readAsText(file);
+    };
+    input.click();
+}
+
+function saveString(contents, type, name) {
+    const blob = new Blob([contents], { type });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = name;
+    a.click();
+
+    URL.revokeObjectURL(url);
+}
