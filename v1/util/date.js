2936bc88ed206e04f7fe10fb0ef7021409f0dfa4
Centrix14 <artiom.zvyagin@yandex.ru>
2026-05-26 21:34:02 +0300

    +fn= wellDate ; возвращает нормальную дату
    Появление этой функции вызвано совершенно уникальной неэргономичностью
    web-спецификации. <input> с типом datetime-local принимает в качестве
    значения только строку, оформленную по специальным правилам (близки к
    ISO).
    
    При этом никаких методов превращения Date в подобного рода строку не
    существует. Самое близкое — toISOString, который придётся ещё
    специально обрезать. Но мало горя: после обрезки получится нужная
    строка в неверном часовом поясе!
    
    (Ясно при этом, что просто засунуть в value значение типа Date
    нельзя.)
    
    Именно поэтому я вслед за огромным числом разработчиков по всему миру
    реализую вот такую ненужную раскоряку.

---
+++ v1/util/date.js
@@ -0,0 +1,9 @@
+function wellDate(date) {
+    const year = date.getFullYear(),
+          month = (date.getMonth()+1).toString().padStart(2, '0'),
+          day = (date.getDate()).toString().padStart(2, '0'),
+          hours = (date.getHours()).toString().padStart(2, '0'),
+          minutes = (date.getMinutes()).toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
